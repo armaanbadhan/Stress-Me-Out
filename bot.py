a6a04655b012e42d41526fa30a5d63304b653199
Rijul24 <rijulsingla.btcse20@pec.edu.in>
2023-02-09 09:10:23 +0530

    code cleaned

--- bot.py
+++ bot.py
@@ -28,7 +28,7 @@ async def ping(interaction: Interaction):
 
 @bot.slash_command(name="stressmeout", description="Shows The Reminders", guild_ids=[TEST_SERVER_ID])
 async def stressmeout(interaction: Interaction):
-    embed = nextcord.Embed(title = "Reminders" , description = "Current reminders of this server" , timestamp = datetime.datetime.utcnow())
+    embed = nextcord.Embed(title = "Reminders", description = "Current reminders of this server", timestamp = datetime.datetime.utcnow())
     
     #remove the dummy data
     dbinteract.delete_deadline(TEST_SERVER_ID)
@@ -49,11 +49,7 @@ async def stressmeout(interaction: Interaction):
     """
         
     for idx in range(len(deadlines)):
-        embed.add_field(
-                name= str(deadlines[idx][0]),
-                value = str(deadlines[idx][1]),
-                inline = False
-        )
+        embed.add_field(name= str(deadlines[idx][0]), value = str(deadlines[idx][1]), inline = False)
         
     #remove the dummy data
     dbinteract.delete_deadline(TEST_SERVER_ID)
@@ -62,31 +58,28 @@ async def stressmeout(interaction: Interaction):
     await interaction.send(embeds=[embed])
 
 @bot.slash_command(name="add", description="add a reminder", guild_ids=[TEST_SERVER_ID])
-async def add(interaction: Interaction , reminder_name: str , date: str , month: str , year: str , hour: str , minutes: str):
+async def add(interaction: Interaction, reminder_name: str, date: str, month: str, year: str, hour: str, minutes: str):
 
     #noting the current time
     curr_time = datetime.datetime.now()
-    curr_time = datetime.datetime.strftime(curr_time , '%d%m%Y %H:%M')
-    curr_time = datetime.datetime.strptime(curr_time , '%d%m%Y %H:%M')
+    curr_time = datetime.datetime.strftime(curr_time, '%d%m%Y %H:%M')
+    curr_time = datetime.datetime.strptime(curr_time, '%d%m%Y %H:%M')
 
-    #20082002 12:05
+    #deadline format: 20082002 12:05
     date = date + month + year
-    deadline = date + " " + hour+ ":" + minutes 
+    deadline = date + " " + hour + ":" + minutes 
     
     try:
+        deadline_time = datetime.datetime.strptime(deadline, "%d%m%Y %H:%M")
 
-        deadline_time = datetime.datetime.strptime(deadline , "%d%m%Y %H:%M")
-
-        if(curr_time>deadline_time):
+        if(curr_time > deadline_time): 
             await interaction.response.send_message("Date has already expired!")
         else:
-            flag = dbinteract.insert_deadline(TEST_SERVER_ID , reminder_name , deadline_time.isoformat())
-            if(flag):
-                await interaction.response.send_message("Successfully added a reminder!")
-            else:
-                await interaction.response.send_message("The name " + reminder_name + " already exists in the table! ")
+            flag = dbinteract.insert_deadline(TEST_SERVER_ID, reminder_name, deadline_time.isoformat())
+            
+            if(flag): await interaction.response.send_message("Successfully added a reminder!")
+            else: await interaction.response.send_message("The name " + reminder_name + " already exists in the table! ")
 
-    
     except ValueError as ve:
         await interaction.response.send_message("The Time is wrong! Please try again.")
 
