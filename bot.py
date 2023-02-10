b290345396991f14338832d6310cfa188e7cadb9
armaan <armaanbadhan07@gmail.com>
2023-02-10 23:50:24 +0530

    removed dummy data things

--- bot.py
+++ bot.py
@@ -29,33 +29,11 @@ async def ping(interaction: Interaction):
 @bot.slash_command(name="stressmeout", description="Shows The Reminders", guild_ids=[TEST_SERVER_ID])
 async def stressmeout(interaction: Interaction):
     embed = nextcord.Embed(title = "Reminders", description = "Current reminders of this server", timestamp = datetime.datetime.utcnow())
-    
-    #remove the dummy data
-    dbinteract.delete_deadline(TEST_SERVER_ID)
-        
-    #add a dummy deadlines
-    dbinteract.insert_deadline(TEST_SERVER_ID, "TASK1", "2023-02-08T11:34:20.753583")
-    dbinteract.insert_deadline(TEST_SERVER_ID, "TASK2", "2023-02-09T11:34:20.753583")
-    
-    #deadlines is a list of tuples
     deadlines = dbinteract.read_deadline(TEST_SERVER_ID)
-    
-    """
-    send the remninder in the form as an discord embed:
-    NAME
-        DATE
-    NAME
-        DATE 
-    """
-        
     for idx in range(len(deadlines)):
         embed.add_field(name= str(deadlines[idx][0]), value = str(deadlines[idx][1]), inline = False)
-        
-    #remove the dummy data
-    dbinteract.delete_deadline(TEST_SERVER_ID)
-    
-    #send the embed
     await interaction.send(embeds=[embed])
+
 
 @bot.slash_command(name="add", description="add a reminder", guild_ids=[TEST_SERVER_ID])
 async def add(interaction: Interaction, reminder_name: str, date: str, month: str, year: str, hour: str, minutes: str):
