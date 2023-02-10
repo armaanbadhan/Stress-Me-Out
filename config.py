28090520f5afff68c373c1e54e52519011eeb1de
armaan <armaanbadhan07@gmail.com>
2023-02-11 00:24:25 +0530

    i need help

--- config.py
+++
@@ -1,9 +0,0 @@
-
-# paste the generated token here
-# MAKESURE TOKEN IS NOVER SHARED
-TOKEN = "dummy_token" 
-
-# copy the server id of the server where you added your bot and paste it here
-# To get the server ID right click on the server -> Copy ID
-# If the copy id option is not visible, turn on developer options in discord settings.
-TEST_SERVER_ID = 1039826459503644704 
